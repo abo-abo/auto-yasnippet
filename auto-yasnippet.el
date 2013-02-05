763009968da4c1ef657dd3d70debba68a39046ae
oleh <wave@mail.ua>
2013-02-05 11:53:32 +0100

    Initial import.

---
+++ auto-yasnippet.el
@@ -0,0 +1,134 @@
+;;; auto-yasnippet.el --- Quickly create disposable yasnippets
+
+;; Author: Oleh Krehel <ohwoeowho@gmail.com>
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; Setup:
+;; 
+;; 1. Download yasnippet http://code.google.com/p/yasnippet/ and set it up.
+;; 2. Put this file into your elisp folder.
+;; 
+;; 3. In your .emacs file:
+;;     (require 'yasnippet)       
+;;     (global-set-key (kbd "H-w") 'create-auto-yasnippet)
+;;     (global-set-key (kbd "H-y") 'expand-auto-yasnippet)
+
+;; Usage:
+;; e.g. in JavaScript write:
+;; 
+;; field~1 = document.getElementById("field~1");
+;; 
+;; Since this just one line,
+;; just call `create-auto-yasnippet' (from anywhere on this line).
+;; The ~ chars disappear, yielding valid code.
+;; `*current-auto-yasnippet-template* becomes:
+;; "field$1 = document.getElementById(\"field$1\");"
+;; Now by calling `expand-auto-snippet' multiple times, you get:
+;; 
+;; field1 = document.getElementById("field1");
+;; field2 = document.getElementById("field2");
+;; field3 = document.getElementById("field3");
+;; fieldFinal = document.getElementById("fieldFinal");
+;; 
+;; e.g. in Java write:
+;; 
+;; class Light~On implements Runnable {
+;;   public Light~On() {}
+;;   public void run() {
+;;     System.out.println("Turning ~on lights");
+;;     light = ~true;
+;;   }
+;; }
+;; 
+;; This differs from the code that you wanted to write only by 4 ~ chars.
+;; Since it's more than one line, select the region and call `create-auto-yasnippet'.
+;; Again, the ~ chars disappear, yielding valid code.
+;; `*current-auto-yasnippet-template* becomes:
+;; "class Light$1 implements Runnable {
+;;   public Light$1() {}
+;;   public void run() {
+;;     System.out.println(\"Turning $2 lights\");
+;;     light = $3;
+;;   }
+;; }"
+;; 
+;; Now by calling `expand-auto-snippet', you can quickly fill in:
+;; class LightOff implements Runnable {
+;;   public LightOff() {}
+;;   public void run() {
+;;     System.out.println("Turning off lights");
+;;     light = false;
+;;   }
+;; }
+(defvar *current-auto-yasnippet-template* ""
+  "Used as snippet body, when 'expand-auto-yasnippet is called")
+(defvar *auto-yasnippet-marker* "~"
+  "Used to mark fields and mirrors.
+Another good option is \\$, if you don't care about LaTeX")
+(defvar *auto-yasnippet-field-regex* "\\([a-z0-9-_]+\\)")
+(defun line-indent-position ()
+  (interactive)
+  (save-excursion
+    (back-to-indentation)
+    (point)))
+(defun current-string-position ()
+  (interactive)
+  (let ((head (if mark-active (region-beginning) (line-indent-position)))
+	(tail (if mark-active (region-end) (line-end-position))))
+    (list (buffer-substring-no-properties head tail)
+	  head
+	  tail)))
+(defun create-auto-yasnippet ()
+  "Works on either the current line, or, if `mark-active', the current region.
+Removes `*auto-yasnippet-marker*' prefixes,
+writes the corresponding snippet to `*current-auto-yasnippet-template*',
+with words prefixed by `*auto-yasnippet-marker*' as fields, and mirrors properly set up."
+  (interactive)
+  (multiple-value-bind (s head tail) (current-string-position)
+    (flet ((parse (in vars out)
+		  (if in
+		      (let ((p (string-match (concat
+					      *auto-yasnippet-marker*
+					      *auto-yasnippet-field-regex*) in)))
+			(if p
+			    (let* ((var (match-string 1 in))
+				   (mult (assoc var vars))
+				   (vars (if mult vars
+					   (cons (cons var (+ 1 (cdar vars)))
+						 vars))))
+			      (parse (substring in (+ p (length var) 1))
+				     vars
+				     (concat out
+					     (substring in 0 p)
+					     "$"
+					     (number-to-string (if mult
+								   (cdr mult)
+								 (cdar vars))))))
+			  (concat out in)))
+		    out)))
+      (setq *current-auto-yasnippet-template* (parse s (list (cons "" 0)) ""))
+      (delete-region head tail)
+      (insert (replace-regexp-in-string *auto-yasnippet-marker* "" s)))))
+(defun expand-auto-yasnippet ()
+  "Inserts the last yasnippet created by `create-auto-yasnippet'"
+  (interactive)
+  (yas/expand-snippet *current-auto-yasnippet-template*))
+(provide 'auto-yasnippet)
+;;; auto-yasnippet.el ends here
+
