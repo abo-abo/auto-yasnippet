956be196beb34cb7b37b9d6d1d3da4de1f1aa67e
oleh <wave@mail.ua>
2013-08-07 18:23:12 +0200

    auto-yasnippet.el: added `aya-create-one-line'

--- auto-yasnippet.el
+++ auto-yasnippet.el
@@ -1,6 +1,10 @@
 ;;; auto-yasnippet.el --- Quickly create disposable yasnippets
 
 ;; Author: Oleh Krehel <ohwoeowho@gmail.com>
+;; URL: https://github.com/abo-abo/auto-yasnippet
+;; Version: 0.2
+
+;; This file is not part of GNU Emacs
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -20,34 +24,34 @@
 ;;; Commentary:
 
 ;; Setup:
-;; 
+;;
 ;; 1. Download yasnippet http://code.google.com/p/yasnippet/ and set it up.
 ;; 2. Put this file into your elisp folder.
-;; 
+;;
 ;; 3. In your .emacs file:
-;;     (require 'yasnippet)       
+;;     (require 'yasnippet)
 ;;     (global-set-key (kbd "H-w") 'aya-create)
 ;;     (global-set-key (kbd "H-y") 'aya-expand)
 
 ;; Usage:
 ;; e.g. in JavaScript write:
-;; 
+;;
 ;; field~1 = document.getElementById("field~1");
-;; 
+;;
 ;; Since this just one line,
 ;; just call `aya-create' (from anywhere on this line).
 ;; The ~ chars disappear, yielding valid code.
 ;; `aya-current' becomes:
 ;; "field$1 = document.getElementById(\"field$1\");"
 ;; Now by calling `aya-expand' multiple times, you get:
-;; 
+;;
 ;; field1 = document.getElementById("field1");
 ;; field2 = document.getElementById("field2");
 ;; field3 = document.getElementById("field3");
 ;; fieldFinal = document.getElementById("fieldFinal");
-;; 
+;;
 ;; e.g. in Java write:
-;; 
+;;
 ;; class Light~On implements Runnable {
 ;;   public Light~On() {}
 ;;   public void run() {
@@ -55,7 +59,7 @@
 ;;     light = ~true;
 ;;   }
 ;; }
-;; 
+;;
 ;; This differs from the code that you wanted to write only by 4 ~ chars.
 ;; Since it's more than one line, select the region and call `aya-create'.
 ;; Again, the ~ chars disappear, yielding valid code.
@@ -67,7 +71,7 @@
 ;;     light = $3;
 ;;   }
 ;; }"
-;; 
+;;
 ;; Now by calling `aya-expand', you can quickly fill in:
 ;; class LightOff implements Runnable {
 ;;   public LightOff() {}
@@ -76,6 +80,21 @@
 ;;     light = false;
 ;;   }
 ;; }
+;;
+;; e.g. in C++ write:
+;; const Point<3> curl(grad[~2][~1] - grad[~1][~2],
+;;
+;; select the region between the paren and the comma and call `aya-create'.
+;;
+;; You can easily obtain the final code:
+
+;; const Point<3> curl(grad[2][1] - grad[1][2],
+;;                     grad[0][2] - grad[2][0],
+;;                     grad[1][0] - grad[0][1]);
+;;
+;; Note how annoying it would be to triple check that the indices match.
+;; Now you just have to check for one line.
+
 (defvar aya-current ""
   "Used as snippet body, when 'aya-expand is called")
 
@@ -83,11 +102,39 @@
   "Used to mark fields and mirrors.
 Another good option is \\$, if you don't care about LaTeX")
 
-(defvar aya-field-regex "\\([a-z0-9-_]+\\)")
+(defvar aya-marker-one-line "$"
+  "Used to mark one mirror for simplistic snippets")
 
-(defvar aya-default-action nil
+(defvar aya-field-regex "\\([A-Za-z0-9-]+\\)"
+  "Defines how the filed looks like.
+With the default [A-Za-z0-9-], Foo_bar will expand to $1_bar.
+But if you set [A-Za-z0-9-_], Foo_bar will expand to $1.")
+
+;; you can chain `aya-create' with a function of your choosing,
+;; e.g. copy current line/region if there's no snippet.
+(defvar aya-default-function nil
   "function to call if no snippet markers were on line / in region.")
-(make-variable-buffer-local 'aya-default-action)
+(make-variable-buffer-local 'aya-default-function)
+
+(defun aya-create-one-line ()
+  "This is a simplistic `aya-create' that creates only one mirror
+ (you can still have as many instances of this mirror as you want).
+It's less flexible than `aya-create', but faster.
+It uses a different marker, which is `aya-marker-one-line'.
+You can use it to quickly generate one-liners such as
+menu.add_item(spamspamspam, \"spamspamspam\")"
+  (interactive)
+  (let* ((beg (line-beginning-position))
+         (end (line-end-position))
+         (line (buffer-substring-no-properties beg (point))))
+    (when (string-match "\\$" line)
+      (setq line
+            (concat
+             (replace-regexp-in-string "\\$" "$1" line)
+             "$1"
+             (buffer-substring-no-properties (point) end)))
+      (delete-region beg end)
+      (yas/expand-snippet line))))
 
 (defun aya-create ()
   "Works on either the current line, or, if `mark-active', the current region.
@@ -95,40 +142,41 @@ Removes `aya-marker' prefixes,
 writes the corresponding snippet to `aya-current',
 with words prefixed by `aya-marker' as fields, and mirrors properly set up."
   (interactive)
-  (let* ((head (if mark-active 
-                   (region-beginning)
-                 (save-excursion (back-to-indentation) (point))))
-         (tail (if mark-active (region-end) (line-end-position)))
-         (s (buffer-substring-no-properties head tail)))
-    (cl-labels ((parse (in vars out)
-		  (if in
-		      (let ((p (string-match (concat
-					      aya-marker
-					      aya-field-regex) in)))
-			(if p
-			    (let* ((var (match-string 1 in))
-				   (mult (assoc var vars))
-				   (vars (if mult vars
-					   (cons (cons var (+ 1 (cdar vars)))
-						 vars))))
-			      (parse (substring in (+ p (length var) 1))
-				     vars
-				     (concat out
-					     (substring in 0 p)
-					     "$"
-					     (number-to-string (if mult
-								   (cdr mult)
-								 (cdar vars))))))
-			  (concat out in)))
-		    out)))
-      (setq aya-current
-	    (replace-regexp-in-string "\\\\" "\\\\\\\\" (parse s (list (cons "" 0)) "")))
-      (if (string-match "\\$" aya-current)
-          (progn
-            (delete-region head tail)
-            (insert (replace-regexp-in-string aya-marker "" s)))
-        ;; try some other useful action if it's defined for current buffer
-        (and (functionp aya-default-action) (funcall aya-default-action))))))
+  (unless (aya-create-one-line)
+    (let* ((head (if mark-active
+                     (region-beginning)
+                   (save-excursion (back-to-indentation) (point))))
+           (tail (if mark-active (region-end) (line-end-position)))
+           (s (buffer-substring-no-properties head tail)))
+      (cl-labels ((parse (in vars out)
+                         (if in
+                             (let ((p (string-match (concat
+                                                     aya-marker
+                                                     aya-field-regex) in)))
+                               (if p
+                                   (let* ((var (match-string 1 in))
+                                          (mult (assoc var vars))
+                                          (vars (if mult vars
+                                                  (cons (cons var (+ 1 (cdar vars)))
+                                                        vars))))
+                                     (parse (substring in (+ p (length var) 1))
+                                            vars
+                                            (concat out
+                                                    (substring in 0 p)
+                                                    "$"
+                                                    (number-to-string (if mult
+                                                                          (cdr mult)
+                                                                        (cdar vars))))))
+                                 (concat out in)))
+                           out)))
+        (setq aya-current
+              (replace-regexp-in-string "\\\\" "\\\\\\\\" (parse s (list (cons "" 0)) "")))
+        (if (string-match "\\$" aya-current)
+            (progn
+              (delete-region head tail)
+              (insert (replace-regexp-in-string aya-marker "" s)))
+          ;; try some other useful action if it's defined for current buffer
+          (and (functionp aya-default-function) (funcall aya-default-function)))))))
 
 (defun aya-expand ()
   "Inserts the last yasnippet created by `aya-create'"
@@ -144,25 +192,20 @@ with words prefixed by `aya-marker' as fields, and mirrors properly set up."
 (defvar *yas-tab*
   "The distance from line beginning where `yas/expand' was called")
 
+(set-default 'yas/fallback-behavior 'return-nil)
+
 (defun aya-open-line ()
   (interactive)
-  (cond ((and (looking-back " ") (looking-at "[\s\n}]+"))
-	 (insert "\n\n")
-	 (indent-according-to-mode)
-	 (forward-line -1)
-	 (indent-according-to-mode))
-	((expand-abbrev))
-	(t
-	 (setq *yas-invokation-point* (point))
-         (setq *yas-invokation-buffer* (current-buffer))
-	 (setq *yas-tab* (- (point) (line-beginning-position)))
-         ;; Try to expand a snippet at a key before point,
-         ;; otherwise delegate to `yas/next-field'
-         (if (or (and (fboundp 'yas/snippets-at-point) (yas/snippets-at-point))
-                 (and (fboundp 'yas--snippets-at-point) (yas--snippets-at-point)))
-             (yas/next-field)
+  (cond ((expand-abbrev))
+
+        ((yas/snippets-at-point)
+         (yas/next-field-or-maybe-expand-1))
+
+        ((progn
+           (setq *yas-invokation-point* (point))
+           (setq *yas-invokation-buffer* (current-buffer))
+           (setq *yas-tab* (- (point) (line-beginning-position)))
            (yas/expand)))))
 
 (provide 'auto-yasnippet)
 ;;; auto-yasnippet.el ends here
-
